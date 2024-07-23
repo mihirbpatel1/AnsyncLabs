5a8d1d5bb499f79bd6af2d810302b58917b0bcb1
mihirbpatel1 <mihirbpatel96@gmail.com>
2024-07-23 09:22:39 -0500

    final changes

--- grocery_list_app/lib/main.dart
+++ grocery_list_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'bloc/grocery_bloc.dart';
 import 'screens/grocery_list_screen.dart';
-import 'bloc/grocery_event.dart'; // Import for LoadGroceries
+import 'bloc/grocery_event.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,8 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => GroceryBloc(prefs: prefs)
-        ..add(LoadGroceries()), // Make sure LoadGroceries is imported
+      create: (context) => GroceryBloc(prefs: prefs)..add(LoadGroceries()),
       child: MaterialApp(
         title: 'Grocery List App',
         theme: ThemeData(
